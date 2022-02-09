99d4ef281d3b3bce546b172780a8925099f68978
Sovius <sovius5@mail.ru>
2022-02-09 21:26:09 +0300

    improvment  Views, add Validators

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,31 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError, CharField
 from .models import User, Articles
+import re
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "email", "password", "is_author")
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = (
+            "email",
+            "password",
+            "is_author"
+        )
+
+        extra_kwargs = {
+            'password': {"style": {"input_type": "password"}, 'write_only': True},
+        }
+
+    def validate(self, data):
+        password = data["password"]
+        if len(password) < 8:
+            raise ValidationError("Password too short!")
+        elif not re.search(r'\d', password):
+            raise ValidationError("Must be at least one number")
+        elif not re.search(r'[!@#$%^&*a-zA-Zа-яА-ЯёЁ]', password):
+            raise ValidationError("Must be at least one char or letter")
+        else:
+            return data
 
 
 class LoginSerializer(ModelSerializer):
@@ -14,14 +33,16 @@ class LoginSerializer(ModelSerializer):
         model = User
         fields = ("email", "password")
 
-class ArticleAuthorSerializer(ModelSerializer):
+class CabinetSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ("email", "id")
+        fields = ("password","is_author", "is_subscriber")
+
 
 class ArticlesSerializer(ModelSerializer):
-    user_id = ArticleAuthorSerializer()
-
     class Meta:
         model = Articles
-        fields = ("article_title", "article_text", "user_id")
+        fields = ("article_title", "article_text", "user_id","is_public")
+        extra_kwargs = {
+            'user_id': {'read_only': True},
+        }
--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
+from rest_framework.serializers import ValidationError
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from rest_framework.response import Response
 
 from .models import User, Articles
-from .serializers import UserSerializer, ArticlesSerializer, LoginSerializer
+from .serializers import UserSerializer, ArticlesSerializer, LoginSerializer, CabinetSerializer
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import login, authenticate
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
@@ -18,16 +19,27 @@ class RegisterView(CreateAPIView):
     serializer_class = UserSerializer
 
     def post(self, request):
-        user = User.objects.create_user(
-            request.data.get("email"),
-            request.data.get("password"),
-            request.data.get("is_author")
-        )
+        serializer = UserSerializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            error = str(e.args[0]).split("\'")[3]
+            return Response({f"Error! {error}"})
+
+        try:
+            user = User.objects.create_user(
+                request.data.get("email"),
+                request.data.get("password"),
+                request.data.get("is_author")
+            )
+        except Exception as e:
+            return Response({"Error! Cannot save user!"})
 
         try:
             login(request, user)
         except Exception as e:
-            return Response({"Error! Cannot sign in!": e.error_list[0]})
+            return Response({"Error! Cannot sign in!"})
         else:
             return redirect(f"/api/cabinet/{user.id}")
 
@@ -47,7 +59,6 @@ class LoginView(APIView):
         if user is not None:
             if user.is_active:
                 login(request, user)
-
                 return redirect(f"/api/cabinet/{user.id}")
             else:
                 return HttpResponse("Your account is not active!")
@@ -60,7 +71,7 @@ class UserCabinet(RetrieveUpdateDestroyAPIView):
 
     authentication_classes = (BasicAuthentication, SessionAuthentication)
     permission_classes = (IsAuthenticated,)
-    serializer_class = UserSerializer
+    serializer_class = CabinetSerializer
     queryset = User.objects.all()
     http_method_names = ['get', 'patch', 'delete']
 
@@ -68,8 +79,28 @@ class UserCabinet(RetrieveUpdateDestroyAPIView):
         return super().get(request) if request.user.id == pk else HttpResponse("You don`t have permissions!")
 
     def patch(self, request, pk):
-        request.data["password"] = make_password(request.data.get("password"))
-        return super().patch(request) if request.user.id == pk else HttpResponse("You don`t have permissions!")
+        if request.user.id == pk:
+            user = User.objects.get(id=pk)
+
+            if request.data.get("password"):
+                user.password = make_password(request.data.get("password"))
+
+            if request.data.get("is_author"):
+                user.is_author = request.data.get("is_author")
+
+            if request.data.get("is_subcscriber"):
+                user.is_subscriber = True if request.data.get("is_subscriber") == "true" else False
+
+            if request.data.get("is_alive"):
+                user.is_alive = True if request.data.get("is_alive") == "true" else False
+
+            try:
+                user.save()
+            except Exception as e:
+                return Response({"Error! Cannot update user!"})
+
+        else:
+            return HttpResponse("You don`t have permissions!")
 
     def delete(self, request, pk):
         return super().delete(request) if request.user.id == pk else HttpResponse("You don`t have permissions!")
@@ -86,6 +117,7 @@ class PublicArticlesView(ListAPIView):
 
 class ArticlesCreateUpdateDestroyView(ListCreateAPIView):
     """Страница редактирования статей"""
+
     authentication_classes = (BasicAuthentication, SessionAuthentication)
     permission_classes = (IsAuthenticated,)
     serializer_class = ArticlesSerializer
@@ -95,34 +127,26 @@ class ArticlesCreateUpdateDestroyView(ListCreateAPIView):
         return Articles.objects.filter(user_id=user)
 
     def post(self, request):
-        if not self.request.user.is_author:
-            return HttpResponse("You don`t have permissions! Become an author!")
-        else:
-            article = Articles(
-                user_id=User.objects.get(id=request.user.id),
-                article_title=request.data.get("article_title"),
-                article_text=request.data.get("article_text"),
-                is_public=True if request.data.get("public") == "true" else False
-            )
-
-            try:
-                article.save()
-            except Exception as e:
-                return Response({"Error! Cannot save an article!": e.error_list[0]})
-            else:
-                return HttpResponse("Created!")
+        article = Articles(
+            user_id=User.objects.get(id=request.user.id),
+            article_title=request.data.get("article_title"),
+            article_text=request.data.get("article_text"),
+            is_public=True if request.data.get("is_public") == "true" else False
+        )
+        article.save()
+        return redirect(f"/api/articles/edit")
 
     def patch(self, request):
         article = Articles.objects.get(id=request.data.get("article_id"))
         if article.user_id == User.objects.get(
                 id=request.user.get("id")
         ):
-            article = Articles(
-                user_id=User.objects.get(id=request.user.get("id")),
-                article_title=request.data.get("title"),
-                article_text=request.data.get("text"),
-                is_public=True if request.data.get("public") == "true" else False
-            )
+            if request.data.get("article_title"):
+                article.article_title = request.data.get("article_title")
+            if request.data.get("article_text"):
+                article.article_text = request.data.get("article_text")
+            if request.data.get("is_public"):
+                article.is_public = True if request.data.get("is_public") == "true" else False
             try:
                 article.save()
             except Exception as e:
