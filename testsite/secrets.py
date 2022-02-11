64e05481728fbd4a0417fa77489c6aae4aeb61ca
Sovius <sovius5@mail.ru>
2022-02-11 20:14:08 +0300

    add secretfile

---
+++ testsite/secrets.py
@@ -0,0 +1,9 @@
+DJANGO_SECRET_KEY='django-insecure-pf(mscy6e(zv+v-=x))-+!^jo!#0bnrp-xslhn2k2@p87y(e_='
+
+DATA_BASE_SETTINGS ={
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'ShowBase',
+    'USER': 'postgres',
+    'PASSWORD': 'admin',
+    'HOST': '127.0.0.1',
+    'PORT': '5432', }
