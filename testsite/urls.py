8d244aa91843a83e0e13c81130b960196b65367f
Sovius <sovius5@mail.ru>
2022-02-13 16:23:32 +0300

    add entry template

--- testsite/urls.py
+++ testsite/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api import urls as urls_api
+from django.views.generic import TemplateView
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
     path("api/", include(urls_api)),
+    path("",TemplateView.as_view(template_name='index.html'))
 
 ]
