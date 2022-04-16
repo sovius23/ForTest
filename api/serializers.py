4ce5fdc47df387401389cef7ad767844ced954b3
sovius23 <79575666+sovius23@users.noreply.github.com>
2022-04-16 11:30:46 +0300

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,24 +1,28 @@
-from django_filters.rest_framework import FilterSet
-
 from .models import Article, Subjects
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, StringRelatedField, SerializerMethodField
 from django.contrib.auth import get_user_model
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
-
+from api.service import calculate_distance
 Profile = get_user_model()
 
 
-class ProfileSerializer(serializers.ModelSerializer):
+class ProfileForArticleSerializer(ModelSerializer):
+    distance= SerializerMethodField()
     class Meta:
         model = Profile
-        fields = "__all__"
+        fields = ("sex","age","longitude","latitude","last_seen","distance")
         extra_kwargs = {
             'password': {"style": {"input_type": "password"}, 'write_only': True},
         }
+        
+        def get_distance(self, obj):
+            profile = self.request.user
+            return calculate_distance(profile.longitude,profile.latitude,obj.longitude,obj.latitude)
+            
 
 
-class RegisterSerializer(serializers.ModelSerializer):
+class RegisterSerializer(ModelSerializer):
     class Meta:
         model = Profile
         fields = ('username', 'password', 'email')
@@ -27,10 +31,19 @@ class RegisterSerializer(serializers.ModelSerializer):
         }
 
 
-class ArticleSerializer(serializers.ModelSerializer):
+class ManyArticlesSerializer(ModelSerializer):
+    text = SerializerMethodField()
+    profile = ProfileForArticleSerializer()
     class Meta:
         model = Article
         fields = "__all__"
-        depth = 1
-
-# обрезка статей расстояние
+        exclude = ["is_public"]
+        def get_text(self,obj):
+            return f"{obj.text[0:100]}..."
+        
+class ArticleSerializer(ModelSerializer):
+    profile = ProfileForArticleSerializer()
+    class Meta:
+        model = Article
+        fields = "__all__"
+        exclude = ["is_public"]
