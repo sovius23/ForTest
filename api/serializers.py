b62c80908b04633f45a16c4d67969bed4d284eda
sovius23 <79575666+sovius23@users.noreply.github.com>
2022-04-18 11:32:30 +0300

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -7,24 +7,11 @@ from api.service import calculate_distance
 Profile = get_user_model()
 
 
-class ProfileForArticleSerializer(ModelSerializer):
-    distance= SerializerMethodField()
-    class Meta:
-        model = Profile
-        fields = ("sex","age","longitude","latitude","last_seen","distance")
-        extra_kwargs = {
-            'password': {"style": {"input_type": "password"}, 'write_only': True},
-        }
-        
-    def get_distance(self, obj):
-        profile = self.context.get("request").user
-        q= calculate_distance.calculate_distance(profile.longitude,profile.latitude,obj.longitude,obj.latitude)
-        return q
-
 class ProfileSerializer(ModelSerializer):
+    photos = StringRelatedField(many=True)
     class Meta:
         model = Profile
-        fields = "__all__"
+        fields = ('username', 'password', 'email','age','latitude','longitude','first_name','last_name','photos')
 
 class RegisterSerializer(ModelSerializer):
     class Meta:
@@ -33,21 +20,39 @@ class RegisterSerializer(ModelSerializer):
         extra_kwargs = {
             'password': {"style": {"input_type": "password"}, 'write_only': True},
         }
+        
+        
+class PublicProfileForArticleSerializer(ModelSerializer):
+    distance= SerializerMethodField()
+    class Meta:
+        model = Profile
+        fields = ("username")
+        
+class AuthorProfileForArticleSerializer(ModelSerializer):
+    distance= SerializerMethodField()
+    class Meta:
+        model = Profile
+        fields = ("sex","age","last_seen","distance",'username')
+    def get_distance(self, obj):
+        profile = self.context.get("request").user
+        q= calculate_distance.calculate_distance(profile.longitude,profile.latitude,obj.longitude,obj.latitude)
+        return q
 
+class ArticlesSerializer(ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('fields', None)
 
-class ManyArticlesSerializer(ModelSerializer):
+        # Instantiate the superclass normally
+        super(DynamicFieldsSerializerMixin, self).__init__(*args, **kwargs)
+        
     text = SerializerMethodField()
     profile = ProfileForArticleSerializer()
     class Meta:
         model = Article
-        fields = "__all__"
         exclude = ["is_public"]
         def get_text(self,obj):
-            return f"{obj.text[0:100]}..."
-        #def get_field_names():
+            if view.one:
+                return obj.text
+            else: return f"{obj.text[0:100]}..."
         
-class ArticleSerializer(ModelSerializer):
-    profile = ProfileForArticleSerializer()
-    class Meta:
-        model = Article
-        exclude = ["is_public"]
